e08ace4d1f0456e98f2d4c242536d269ffb4b2f5
sebastian8080 <sebas25211@hotmail.com>
2021-01-26 00:19:02 -0500

    Arreglando los cors

--- public/js/ejemplo.js
+++ public/js/ejemplo.js
@@ -64,7 +64,7 @@ const cuestionary = [
 
 document.querySelector('#h1animales').innerHTML = titulo;
 
-const socket = io('http://localhost:3000');
+const socket = io.connect('http://localhost:3000');
 
 socket.on('data', function(data){
     if(data != null){
